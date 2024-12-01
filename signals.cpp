b691309af8349438abf155c0a3db3f5a63bef07b
tomer <tomerguttman@campus.technion.ac.il>
2024-12-01 15:20:01 +0200

    signal (ctrl+z/c) handlers functions

--- signals.cpp
+++ signals.cpp
@@ -4,18 +4,26 @@
 #include <csignal>
 #include <unistd.h>
 #include <sys/types.h>
+#include "smash.cpp"
 
 using namespace std;
 
 // Signal handler function for Ctrl+Z
 void sigtstpHandler(int sig) {
-    //sigstp for the front process
-    cout << "caught CTRL+Z" << endl;
+    cout << "smash: caught CTRL+Z" << endl;
+    if (my_os.fg_process) {
+        kill(my_os.fg_process.pid , SIGSTOP);
+        cout << "smash: proccess " << my_os.fg_process.pid << " was stopped" << endl;
+    }
 }
 
 // Signal handler function for Ctrl+C
 void sigintHandler(int sig) {
     cout << "caught CTRL+C" << endl;
+    if (my_os.fg_process) {
+      kill(my_os.fg_process.pid , SIGKILL);
+      cout << "smash: proccess " << my_os.fg_process.pid << " was killed" << endl;
+    }
 }
 
 //function that register signal handlers
